a4eebaed993aada37b84f6ff2afc6c20968c12c3
dafnye <dafny.lira@aluno.faculdadeimpacta.com.br>
2025-03-25 17:38:50 -0300

    infraestrutura de persistencia do banco

--- src/Domain/user.py
+++ src/Domain/user.py
@@ -1,12 +1,24 @@
-class UserDomain:
-    def __init__(self, name, email, password):
+class SellerDomain:
+    def init(self, id, name, cnpj, email, phone, password,
+            status="Inativo", activation_code=None, role="Cliente"):
+        self.id = id
         self.name = name
+        self.cnpj = cnpj
         self.email = email
+        self.phone = phone
         self.password = password
+        self.status = status
+        self.activation_code = activation_code
+        self.role = role
 
     def to_dict(self):
         return {
+            "id": self.id,
             "name": self.name,
+            "cnpj": self.cnpj,
             "email": self.email,
-            "password": self.password
+            "phone": self.phone,
+            "status": self.status,
+            "activation_code": self.activation_code,
+            "role": self.role
         }
--- src/Infrastructure/Models/user.py
+++ src/Infrastructure/Models/user.py
@@ -1,17 +1,28 @@
 from src.Config.data_base import db
+from src.Domain.user import SellerDomain
 
 
-class User(db.Model):
-    __tablename__ = 'users'
+class Seller(db.Model):
+    tablename = 'sellers'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    cnpj = db.Column(db.String(18), unique=True, nullable=True)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    phone = db.Column(db.String(15), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    status = db.Column(db.String(10), default="Inativo")
+    activation_code = db.Column(db.String(6), nullable=True)
+    role = db.Column(db.String(10), nullable=False, default="Vendedor")
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "email": self.email,
-            "password": self.password
-        }
+    def to_domain(self):
+        return SellerDomain(
+            id=self.id,
+            name=self.name,
+            cnpj=self.cnpj,
+            email=self.email,
+            phone=self.phone,
+            password=self.password,
+            status=self.status,
+            activation_code=self.activation_code,
+            role=self.role
+        )
