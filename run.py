35309e629187281c53506cac142599e1975030b0
FrankRibe <ribeirofrank65@gmail.com>
2025-05-15 22:20:15 -0300

    feat(product): cria produto e aumenta JWT para 60min
    - Adiciona funcionalidade para criação de produto associada ao seller autenticado.
    - Configura tempo de expiração do JWT para 60 minutos para melhor experiência do usuário.
    - Garante validação dos dados obrigatórios no endpoint de criação.

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=10)
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
